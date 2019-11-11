2c35c38be65c7aef6d1d81d2f686b7d55e0dd472
Zsrrrrr <55135131+Zsrrrrr@users.noreply.github.com>
2019-11-11 14:38:26 +0800

    test

--- src/components/BandEvent.jsx
+++ src/components/BandEvent.jsx
@@ -19,14 +19,21 @@ class BindEvent extends Component {
                 {/* onClick只接受 function 作为处理函数 */}
                 {/* 箭头函数 本身就是一个匿名的 function 函数 */}
                 {/* 箭头函数 谁调用指向谁 */}
-                <button onClick={ ()=>{ this.myClickHandler("hhh") } }>按钮</button>
+                <button onClick={ ()=>{ this.myClickHandler("000000") } }>按钮</button>
             </div>
         )
     }
 
 
     myClickHandler=(arg1)=>{
-        console.log("222222"+arg1);
+
+        /**
+         * 在 React 中，如果想为state中的数据重新赋值
+         * 应该调用 React 提供的this.setState
+         */
+        this.setState({
+            msg:'123' + arg1
+        })
     }
 } 
 
